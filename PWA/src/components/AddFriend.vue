f0dd73bff605114f7678ac031faf74e97db216f7
Margodt Miguel <miguel.margodt@student.howest.be>
2018-11-14 17:31:43 +0100

    friends page styling

---
+++ PWA/src/components/AddFriend.vue
@@ -0,0 +1,80 @@
+<template>
+    <div class="c-addfriend">
+        <div class="c-addfriend__close">
+            <div class="c-addfriend__close__title">
+                Add friend
+            </div>
+            <div v-on:click.prevent="closePanel" class="c-addfriend__close__button">
+                <img src="../assets/cross.png" alt="">
+            </div>
+        </div>
+        <input id="friendToAdd" placeholder="Enter friends username" type="text" autocomplete="off" required/>
+
+        <button type="submit" class="c-button-primary">SEND INVITE</button>
+    </div>
+</template>
+
+<style lang="scss">
+ @import './src/style/settings/settings.colors.scss';
+    .c-addfriend{
+        background:$bg-color;
+        height:100vh;
+    }
+
+    .c-addfriend__close img{
+        width:30px;
+        margin:20px;
+    }
+
+    .c-addfriend__close{
+        width:100%;
+        display:flex;
+        justify-content: space-between;
+    }
+    
+    .c-addfriend__close__button:hover{
+        cursor: pointer;
+    }
+
+    .c-addfriend input{
+        width:85%;
+        border:0;
+        border-bottom:1px $alpha-color solid;
+        background:none;
+        color:$alpha-color;
+        font-size:1.3em;
+        margin-top:40px;
+        margin-bottom:40px;
+        margin-left:7%;
+    }
+
+    .c-addfriend input:focus{
+        outline:none;
+    }   
+
+    .c-addfriend__close__title{
+        margin-left:7%;
+        font-size:1.5em;
+        font-weight:600;
+        line-height:70px;
+    }
+    
+</style>
+
+<script>
+export default {
+    name:'addfriend',
+    data(){
+        return{
+
+        };
+    },
+    methods:{
+        closePanel() {
+            this.$emit("closePanel", {});
+        }
+    }
+}
+</script>
+
+
--- PWA/src/views/Friends.vue
+++ PWA/src/views/Friends.vue
@@ -1,28 +1,74 @@
 <template>
+<div>
+    <slideout-panel>
+
+    </slideout-panel>
+
     <div class="c-friends">
         <div class="c-friends__header">
             <div class="c-friends__header__item">
                 <h1>Friends</h1>
             </div>
             <div class="c-friends__header__item">
-                <img src="../assets/addFriend.png" alt="">
+                <img v-on:click.prevent="showPanel" src="../assets/addFriend.png" alt="">
+            </div>
+        </div>
+
+        <h2>Pending</h2>
+        <div class="c-friends__list">
+            <div class="c-friends__list__item">
+                <p>Bobba</p>
+            </div>
+            <div class="c-friends__list__item">
+                <img src="../assets/checkmark.png" alt="">
+                <img src="../assets/cross.png" alt="">
+            </div>
+        </div>
+        <div class="c-friends__list">
+            <div class="c-friends__list__item">
+                <p>Bobba</p>
+            </div>
+            <div class="c-friends__list__item">
+                <img src="../assets/checkmark.png" alt="">
+                <img src="../assets/cross.png" alt="">
+            </div>
+        </div>
+
+        <h2>Friendlist</h2>
+        <div class="c-friends__list">
+            <div class="c-friends__list__item">
+                <p>Bobba</p>
+            </div>
+            <div class="c-friends__list__item">
+                <img src="../assets/trash.png" alt="">
+            </div>
+        </div>
+        <div class="c-friends__list">
+            <div class="c-friends__list__item">
+                <p>Bobba</p>
+            </div>
+            <div class="c-friends__list__item">
+                <img src="../assets/trash.png" alt="">
             </div>
         </div>
 
         <router-link to="/">    
-            <div class="c-button-primary secundary">PICK OTHER MAP</div>
+            <div class="c-button-primary secundary">BACK TO MENU    </div>
         </router-link>
     </div>
+</div>
 </template>
 
 <style lang="scss">
+
+    .c-friends{
+        width:80%;
+        margin:auto;
+    }
     .c-friends__header{
         display:flex;
         justify-content: space-between;
-    }
-
-    .c-friends__header__item{
-        margin:40px;
+        padding:40px 0px;
     }
 
     .c-friends__header__item h1{
@@ -33,12 +79,77 @@
     .c-friends__header__item img{
         margin-top:6px;
         width:40px;
+        transition: transform .2s ease-in-out;
     }
+
+    .c-friends__header__item img:hover{
+        cursor: pointer;
+        transform: scale(1.2); 
+    }
+
+    .c-friends h2{
+        font-size:2em;
+        font-weight:600;
+        margin:10px 0px;
+    }
+
+    .c-friends__list{
+        display:flex;
+        justify-content: space-between;
+    }
+
+    .c-friends__list__item{
+        margin:3px 0px;
+    }
+
+    .c-friends__list__item img{
+        width:24px;
+        margin:0px 5px;
+        transition: transform .2s ease-in-out;
+    }
+
+    .c-friends__list__item img:hover{
+        cursor: pointer;
+        transform: scale(1.2);
+    }
+
+    .c-friends__list__item p{
+        font-size:1.3em;
+    }
+
+    .c-friends .secundary{
+        margin-top:20px;
+    }
+
+    @media only screen and (max-width:600px){
+    .c-friends{
+        width: 95%;
+    }
+}
 </style>
 
 <script>
+import { VueSlideoutPanel } from 'vue2-slideout-panel';
+import addfriend from '../components/AddFriend';
+import { vueSlideoutPanelService } from 'vue2-slideout-panel';
+
 export default {
-    name:'friends'
+    name:'friends',
+    components:{
+        'slideout-panel': VueSlideoutPanel
+    },
+    methods:{
+        showPanel(){
+            const panel1Handle = vueSlideoutPanelService.show({
+            component : addfriend,
+            width: '350px',
+            props: {
+                //any data you want passed to your component
+            }
+        })
+
+        }
+    }
 }
 </script>
 
