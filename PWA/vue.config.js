e4ad3d7cff4383622ef4db3152022e9c0064ca0c
Margodt Miguel <miguel.margodt@student.howest.be>
2018-11-16 09:58:50 +0100

    icons

--- PWA/vue.config.js
+++ PWA/vue.config.js
@@ -1,5 +1,12 @@
 module.exports = {
     baseUrl: process.env.NODE_ENV === 'production'
-      ? '/geopoint/'
-      : '/'
+      ? '/geopoint2/'
+      : '/',
+      pwa: {
+        name: 'GeoPoint',
+        themeColor: '#16be99',
+        msTileColor: '#000000',
+        appleMobileWebAppCapable: 'yes',
+        appleMobileWebAppStatusBarStyle: '#16be99',
+      }
   }
