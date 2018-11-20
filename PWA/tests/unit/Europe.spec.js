97ce17c55720e296a949cb8f9ce2d060f8d8c545
Margodt Miguel <miguel.margodt@student.howest.be>
2018-11-20 21:09:33 +0100

    added 2 more unit tests

--- PWA/tests/unit/Europe.spec.js
+++ PWA/tests/unit/Europe.spec.js
@@ -4,6 +4,12 @@ import Vue from 'vue'
 import Europe from '@/components/Europe.vue'
 
 describe('Europe.vue',()=>{
+    it('score should be 0 at start',() =>{
+        const Constructor = Vue.extend(Europe)
+        const vm = new Constructor().$mount()
+        expect(vm.score).to.equal(0);
+    })
+
     it('increments count when correct country was clicked',() =>{
         const Constructor = Vue.extend(Europe)
         const vm = new Constructor().$mount()
@@ -16,6 +22,12 @@ describe('Europe.vue',()=>{
         const vm = new Constructor().$mount()
         var randomCountry = vm.pickRandomCountry({random1:'Country1'})
         expect(randomCountry).to.equal('random1');
+    })
+
+    it('should show a correct country from the start',() =>{
+        const Constructor = Vue.extend(Europe)
+        const vm = new Constructor().$mount()
+        expect(vm.corrCountry).not.to.equal('');
     })
 })
 
